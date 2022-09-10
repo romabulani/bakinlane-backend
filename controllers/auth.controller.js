d8fc73f313104b4d9f34b55437e4e1fa02793032
Roma Bulani <42478246+romabulani@users.noreply.github.com>
2022-09-10 21:16:12 +0530

    fix: multiple responses

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,64 +3,63 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user.model");
 
 const signupHandler = async (req, res) => {
-  const data = req.body;
-  let userFound;
-
   try {
+    const data = req.body;
+    let userFound;
     userFound = await User.findOne({ email: data.email });
+    if (userFound)
+      return res.status(409).json({ message: "User already exists." });
+
+    let encrptedPassword;
+    try {
+      encrptedPassword = await bcrypt.hash(data.password, 15);
+    } catch (e) {
+      return res
+        .status(500)
+        .json({ message: "Signup failed.Please try again later!" });
+    }
+
+    const createdUser = new User({
+      ...data,
+      password: encrptedPassword,
+      cart: [],
+      orders: [],
+      address: [],
+      wishlist: [],
+    });
+
+    try {
+      await createdUser.save();
+    } catch (e) {
+      return res
+        .status(500)
+        .json({ message: "Signup failed.Please try again later!" });
+    }
+
+    const token = jwt.sign(
+      { userId: createdUser._id, email: createdUser.email },
+      process.env.SECRET_KEY,
+      { expiresIn: "2h" }
+    );
+    res.status(201).json({
+      message: "Signup successful",
+      user: {
+        token,
+        _id: createdUser._id,
+        firstName: createdUser.firstName,
+        lastName: createdUser.lastName,
+        email: createdUser.email,
+        cart: createdUser.cart,
+        orders: createdUser.orders,
+        address: createdUser.address,
+        wishlist: createdUser.wishlist,
+      },
+    });
   } catch (e) {
     return res
       .status(500)
       .json({ message: "Signup failed.Please try again later!" });
   }
-
-  if (userFound) res.status(409).json({ message: "User already exists." });
-
-  let encrptedPassword;
-  try {
-    encrptedPassword = await bcrypt.hash(data.password, 15);
-  } catch (e) {
-    return res
-      .status(500)
-      .json({ message: "Signup failed.Please try again later!" });
-  }
-
-  const createdUser = new User({
-    ...data,
-    password: encrptedPassword,
-    cart: [],
-    orders: [],
-    address: [],
-    wishlist: [],
-  });
-
-  try {
-    await createdUser.save();
-  } catch (e) {
-    return res
-      .status(500)
-      .json({ message: "Signup failed.Please try again later!" });
-  }
-
-  const token = jwt.sign(
-    { userId: createdUser._id, email: createdUser.email },
-    process.env.SECRET_KEY,
-    { expiresIn: "2h" }
-  );
-  res.status(201).json({
-    message: "Signup successful",
-    user: {
-      token,
-      _id: createdUser._id,
-      firstName: createdUser.firstName,
-      lastName: createdUser.lastName,
-      email: createdUser.email,
-      cart: createdUser.cart,
-      orders: createdUser.orders,
-      address: createdUser.address,
-      wishlist: createdUser.wishlist,
-    },
-  });
 };
 
 const loginHandler = async (req, res) => {
@@ -87,7 +86,7 @@ const loginHandler = async (req, res) => {
       .json({ message: "Login failed.Please try again later!" });
   }
   if (!isPasswordValid)
-    res.status(401).json({
+    return res.status(401).json({
       message: "Invalid credentials. Check your username and password.2",
     });
 
@@ -96,7 +95,7 @@ const loginHandler = async (req, res) => {
     process.env.SECRET_KEY,
     { expiresIn: "2h" }
   );
-  res.status(200).json({
+  return res.status(200).json({
     message: "Login successful",
     user: {
       token,
